f31116e2360e2ab61ef19179631f3effe9096e73
I524303 <w.zakrzewski@student.fontys.nl>
2025-12-30 02:47:16 +0100

    navbar fixes

--- script.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector(".header-wrapped");
-
-function toggleHeader() {
-  if (window.scrollY > 10) header.classList.add("is-visible");
-  else header.classList.remove("is-visible");
-}
-
-window.addEventListener("scroll", toggleHeader);
-toggleHeader(); // run once on load
